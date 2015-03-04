32e9e78e329b9c660d8b2813f0a729749a78796e
IvanSherman <ivansherman@me.com>
2015-03-04 11:23:19 -0500

    new header, and updated -b

--- rails/etsy_product_catalog/app/controllers/sessions_controller.rb
+++ rails/etsy_product_catalog/app/controllers/sessions_controller.rb
@@ -8,13 +8,6 @@ class SessionsController < ApplicationController
   end 
 
   def authorize
-<<<<<<< HEAD
-    access_token = Etsy.access_token(session[:request_token], 
-      session[:request_secret], 
-      params[:oauth_verifier])
-    user = Etsy.myself(access_token.token, access_token.secret)
-    User.create(etsy_user_name: user.result['login_name'], access_token: user.token, access_token: user.secret)
-=======
     access_token = Etsy.access_token(
     session[:request_token],
     session[:request_secret],
@@ -23,7 +16,6 @@ class SessionsController < ApplicationController
     User.find_by_username(user.result['login_name']) || User.create(username: user.result['login_name'], access_token: user.token, access_secret: user.secret, etsy_user_id: user.result['user_id'])
     session[:user_id] = user.id 
     redirect_to root_url
->>>>>>> de8d4f2bccd4c32f8e5e719b3d46ba35410fe722
   end
 
 
--- rails/etsy_product_catalog/app/controllers/user_controller.rb
+++ rails/etsy_product_catalog/app/controllers/user_controller.rb
@@ -1,8 +1,3 @@
 class UserController < ApplicationController
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> de8d4f2bccd4c32f8e5e719b3d46ba35410fe722
 end
